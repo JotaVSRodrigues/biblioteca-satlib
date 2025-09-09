d88a5cc64f1e235ed783532f32ee6c4c672a58dc
joaor <joao.rdg119@gmail.com>
2025-09-08 22:05:46 -0300

    first commit -> related to configs and directory organization

---
+++ satlib-back/satlib/src/main/java/com/example/satlib/SatlibApplication.java
@@ -0,0 +1,13 @@
+package com.example.satlib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatlibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatlibApplication.class, args);
+	}
+
+}
---
+++ satlib-back/satlib/src/test/java/com/example/satlib/SatlibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.satlib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatlibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
